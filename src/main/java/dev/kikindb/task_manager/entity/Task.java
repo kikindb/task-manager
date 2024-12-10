e7e43595c6063c4de8a8c2f813f2ad30acde6a6a
= <=>
2024-12-10 15:56:54 -0600

    fix: optimize imports

--- src/main/java/dev/kikindb/task_manager/entity/Task.java
+++ src/main/java/dev/kikindb/task_manager/entity/Task.java
@@ -1,8 +1,13 @@
 package dev.kikindb.task_manager.entity;
 
 import dev.kikindb.task_manager.model.TaskStatus;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.time.LocalDateTime;
 
