77e982eae67b45e6332270f90fc17abfbb6c9250
= <=>
2024-12-11 00:07:49 -0600

    chore: authenticate simple functionality

--- src/main/java/dev/kikindb/task_manager/controller/AuthController.java
+++ src/main/java/dev/kikindb/task_manager/controller/AuthController.java
@@ -1,6 +1,8 @@
 package dev.kikindb.task_manager.controller;
 
 import dev.kikindb.task_manager.entity.User;
+import dev.kikindb.task_manager.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+  @Autowired
+  private AuthService authService;
 
   @PostMapping
   public ResponseEntity<User> authenticate(@RequestBody User user) {
@@ -22,7 +29,9 @@ public class AuthController {
     HttpHeaders headers = new HttpHeaders();
     headers.set("x-auth-token", token);
 
+    Optional<User> authUser = authService.authenticate(user);
+
     // Return the ResponseEntity with the user and headers
-    return new ResponseEntity<>(user, headers, HttpStatus.OK);
+    return new ResponseEntity<>(authUser.get(), headers, HttpStatus.OK);
   }
 }
