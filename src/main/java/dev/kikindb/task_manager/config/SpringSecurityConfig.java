6c2bcf925b09691435c21296924d8bf0764d993c
= <=>
2024-12-16 19:12:03 -0600

    chore: jwt config init

--- src/main/java/dev/kikindb/task_manager/config/SpringSecurityConfig.java
+++ src/main/java/dev/kikindb/task_manager/config/SpringSecurityConfig.java
@@ -3,9 +3,7 @@ package dev.kikindb.task_manager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,11 +12,6 @@ import java.util.Arrays;
 
 @Configuration
 public class SpringSecurityConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     UrlBasedCorsConfigurationSource corsConfigurationSource() {
@@ -37,12 +30,61 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .headers(headers -> headers
                 .frameOptions(frameOptions -> frameOptions.sameOrigin()) // Allow same-origin framing
             )
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .csrf(csrf -> csrf.disable()) // Optionally disable CSRF if it's not needed
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+            .csrf(csrf -> csrf.disable()); // Optionally disable CSRF if it's not needed
+            //.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return httpSecurity.build();
     }
+
+//    @Bean
+//    public DataSource dataSource() {
+//        return new EmbeddedDatabaseBuilder()
+//            .setType(EmbeddedDatabaseType.H2)
+//            .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+//            .build();
+//    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        var admin = User.withUsername("iscjedb@gmail.com")
+//            .password("12345")
+//            .passwordEncoder(str -> passwordEncoder().encode(str))
+//            .roles("ADMIN")
+//            .build();
+//
+//        var user = User.withUsername("diana@gmail.com")
+//            .password("12345")
+//            .passwordEncoder(str -> passwordEncoder().encode(str))
+//            .roles("USER")
+//            .build();
+//
+//        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//       jdbcUserDetailsManager.createUser(admin);
+//       jdbcUserDetailsManager.createUser(user);
+//
+//        return jdbcUserDetailsManager;
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public KeyPair keyPair() {
+//      KeyPairGenerator keyPairGenerator = null;
+//      try {
+//        keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+//      } catch (NoSuchAlgorithmException e) {
+//        throw new RuntimeException(e);
+//      }
+//      keyPairGenerator.initialize(2048);
+//        return keyPairGenerator.generateKeyPair();
+//    }
+
 }
