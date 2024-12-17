3560f3047c96ccfc409b3bcce4ed5cf5b243eb75
= <=>
2024-12-16 19:08:54 -0600

    chore: change task entity

--- src/main/java/dev/kikindb/task_manager/entity/Task.java
+++ src/main/java/dev/kikindb/task_manager/entity/Task.java
@@ -8,10 +8,16 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
 
   @Id
@@ -36,96 +42,4 @@ public class Task {
 
   @Column(name = "updated_at")
   private LocalDateTime updatedAt;
-
-  public Task() {
-  }
-
-  public Task(String id, String title, String body, String authorName, TaskStatus status, String userId, LocalDateTime createdAt, LocalDateTime updatedAt) {
-    this.id = id;
-    this.title = title;
-    this.body = body;
-    this.authorName = authorName;
-    this.status = status;
-    this.userId = userId;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getBody() {
-    return body;
-  }
-
-  public void setBody(String body) {
-    this.body = body;
-  }
-
-  public String getAuthorName() {
-    return authorName;
-  }
-
-  public void setAuthorName(String authorName) {
-    this.authorName = authorName;
-  }
-
-  public TaskStatus getStatus() {
-    return status;
-  }
-
-  public void setStatus(TaskStatus status) {
-    this.status = status;
-  }
-
-  public String getUserId() {
-    return userId;
-  }
-
-  public void setUserId(String userId) {
-    this.userId = userId;
-  }
-
-  public LocalDateTime getCreatedAt() {
-    return createdAt;
-  }
-
-  public void setCreatedAt(LocalDateTime createdAt) {
-    this.createdAt = createdAt;
-  }
-
-  public LocalDateTime getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setUpdatedAt(LocalDateTime updatedAt) {
-    this.updatedAt = updatedAt;
-  }
-
-  @Override
-  public String toString() {
-    return "Task{" +
-        "id='" + id + '\'' +
-        ", title='" + title + '\'' +
-        ", body='" + body + '\'' +
-        ", authorName='" + authorName + '\'' +
-        ", status=" + status +
-        ", userId='" + userId + '\'' +
-        ", createdAt=" + createdAt +
-        ", updatedAt=" + updatedAt +
-        '}';
-  }
 }
